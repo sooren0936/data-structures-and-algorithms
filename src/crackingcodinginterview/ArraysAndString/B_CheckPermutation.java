9b13848abf4a1b33c67b68835eb48ad172e053f3
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2023-03-26 20:45:39 +0300

    Check Permutation: clean up

--- src/crackingcodinginterview/ArraysAndString/B_CheckPermutation.java
+++ src/crackingcodinginterview/ArraysAndString/B_CheckPermutation.java
@@ -14,19 +14,14 @@ public class B_CheckPermutation {
         String string = "ffasdfaassdd";
         String substring = "asdfaassddff";
 
-        System.out.println(solution1(string, substring));
-        System.out.println(solution2(string, substring));
-        System.out.println(solution3(string, substring));
+        System.out.println(solutionWithContains(string, substring));
+        System.out.println(solutionIterativeWithStringArrays(string, substring));
+        System.out.println(solutionIterativeWithCharArrays(string, substring));
     }
 
-    public static boolean solution1(String string, String substring) {
-        if (string == null || string.isBlank()) {
-            return false;
-        }
-
-        if (substring == null || substring.isBlank()) {
-            return false;
-        }
+    public static boolean solutionWithContains(final String string, final String substring) {
+        validateParameter(string);
+        validateParameter(substring);
 
         if (string.length() != substring.length()) {
             return false;
@@ -40,30 +35,25 @@ public class B_CheckPermutation {
         return String.valueOf(chars1).contains(String.valueOf(chars2));
     }
 
-    public static boolean solution2(String string, String substring) {
-        if (string == null || string.isBlank()) {
-            return false;
-        }
-
-        if (substring == null || substring.isBlank()) {
-            return false;
-        }
+    public static boolean solutionIterativeWithStringArrays(final String string, final String substring) {
+        validateParameter(string);
+        validateParameter(substring);
 
         if (string.length() != substring.length()) {
             return false;
         }
 
+        final String[] splittedString = string.split("");
+        final String[] splittedSubstring = substring.split("");
+        final Map<String, Integer> countOfLettersFromString = Arrays.stream(splittedString)
+                .collect(Collectors.toMap(x -> x, String::length, Integer::sum));
 
-        String[] split1 = string.split("");
-        String[] split2 = substring.split("");
-        Map<String, Integer> collect = Arrays.stream(split1).collect(Collectors.toMap(x -> x, String::length, Integer::sum));
-
-        for (String s : split2) {
-            if (collect.get(s) != null) {
-                if (collect.get(s) == 1) {
-                    collect.remove(s);
+        for (final String substr : splittedSubstring) {
+            if (countOfLettersFromString.get(substr) != null) {
+                if (countOfLettersFromString.get(substr) == 1) {
+                    countOfLettersFromString.remove(substr);
                 } else {
-                    collect.put(s, collect.get(s) - 1);
+                    countOfLettersFromString.put(substr, countOfLettersFromString.get(substr) - 1);
                 }
             } else {
                 return false;
@@ -73,35 +63,34 @@ public class B_CheckPermutation {
         return true;
     }
 
-    public static boolean solution3(String string, String substring) {
-        if (string == null || string.isBlank()) {
-            return false;
-        }
-
-        if (substring == null || substring.isBlank()) {
-            return false;
-        }
+    public static boolean solutionIterativeWithCharArrays(final String string, final String substring) {
+        validateParameter(string);
+        validateParameter(substring);
 
         if (string.length() != substring.length()) {
             return false;
         }
 
-        int[] x = new int[128];
+        final int[] charsFromString = new int[128];
 
-        char[] stringChars = string.toCharArray();
-        for (char stringChar : stringChars) {
-            x[stringChar] = x[stringChar] + 1;
+        final char[] stringChars = string.toCharArray();
+        for (final char stringChar : stringChars) {
+            charsFromString[stringChar] = charsFromString[stringChar] + 1;
         }
 
-        char[] chars = substring.toCharArray();
-
-        for (int i = 0; i < chars.length; i++) {
-            char aChar = chars[i];
-            if (x[aChar]-- == 0) {
+        final char[] subStringChars = substring.toCharArray();
+        for (final char aChar : subStringChars) {
+            if (charsFromString[aChar]-- == 0) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private static void validateParameter(String substring) {
+        if (substring == null || substring.isBlank()) {
+            throw new RuntimeException("String cannot be null or empty");
+        }
     }
 }
